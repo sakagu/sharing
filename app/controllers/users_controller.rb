3f44bd7805773aea826cc084024eba37f9794077
sakagu <sakatoshi10416@gmail.com>
2020-06-15 20:25:08 +0900

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,8 +14,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :company)
-    # .merge(authority:1)
+    params.require(:user).permit(:name, :email, :company, :authority).merge(authority:0)
   end
 
 
