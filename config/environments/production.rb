8a64bec901633db6ef87e98ff8bbb34beaec5cdd
sakagu <sakatoshi10416@gmail.com>
2020-06-16 06:29:56 +0900

    add

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,6 +75,20 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.action_mailer.default_url_options = {  :host => 'http://3.114.46.12/'}
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    port: 587,
+    address: 'smtp.gmail.com',
+    domain: 'gmail.com',
+    user_name: 'sakatoshi10416@gmail.com',
+    password: 'bsxmhjhlfrpbixdk',
+    enable_starttls_auto: true
+}
+
+
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
